355abebd8d2b0abbe9b2c4a0e15d00c77c4ecfbf
nkorotkyi <zipz211_kmo@student.ztu.edu.ua>
2022-11-10 15:29:42 +0200

    Rewrite 1-2-3 lines

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -6,9 +6,9 @@ namespace Task1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-            Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-            Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
+            Console.WriteLine("Рядок 1");
+            Console.WriteLine("Рядок 2");
+            Console.WriteLine("Рядок 3");
             Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
             Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
         }
